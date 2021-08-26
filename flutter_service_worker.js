a14bb2b2def36d56eba6274e07b7f6039b80c778
ashank96 <bharatiashank1996@gmail.com>
2021-08-26 10:38:24 +0530

    update native mode

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -7,7 +7,7 @@ const RESOURCES = {
 "native_bridge.js": "3ab3a7e0ed1e5a32bd5b9b32f876052c",
 "index.html": "f509f1b9e9a476b695a9e865e40edd6f",
 "/": "f509f1b9e9a476b695a9e865e40edd6f",
-"main.dart.js": "368703b752698932972d42732946f10c",
+"main.dart.js": "aeb17f71922cd6ba17db274bb14cb990",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "manifest.json": "b2da32757322e11f0e061b28321b83b1",
